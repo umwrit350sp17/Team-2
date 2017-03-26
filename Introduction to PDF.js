5f1895144c34cdad922f6268ed5d6d6db0c2bda5
smsandro <smsandro@go.olemiss.edu>
2017-03-26 18:18:50 -0500

    Update Introduction to PDF.js

--- Introduction to PDF.js
+++ Introduction to PDF.js
@@ -1,2 +1,3 @@
 
-PDF.js is an software orginally embedded within the code of Firefox. For this tutorial, we will show how to download and use the Google Chrome Extension. 
+PDF.js is an software orginally embedded within the code of Firefox. 
+For this tutorial, we will show how to download and use the Google Chrome Extension. 
