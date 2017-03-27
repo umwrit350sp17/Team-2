fd04ae202a06538b8277f5baaaef0fb6ff439a47
smsandro <smsandro@go.olemiss.edu>
2017-03-27 15:49:15 -0500

    Update How to Download PDF.js

--- teamdocs/Downloading PDF.js for Google Chrome/How to Download PDF.js
+++ teamdocs/Downloading PDF.js for Google Chrome/How to Download PDF.js
@@ -1,4 +1,4 @@
 Before trying to download PDF.js as a Google Chrome Extension, make sure that you have Chrome installed on your desktop. 
 
 Step One: Access the Google Chrome Web Store and search for "PDF.js" in the search bar. 
-![This is what your screen should look like after you have searched "PDF.js"](picture.png)
+![This is what your screen should look like after you have searched "PDF.js"](Screen Shot 2017-03-26 at 6.23.44 PM.png)
